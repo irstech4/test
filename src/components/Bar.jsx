c78c527f1b8504c619a3c4a40c9641ed63c731c8
IRS-Github <irspvtltd7@gmail.com>
2024-03-19 11:42:44 +0530

    first commit

---
+++ src/components/Bar.jsx
@@ -0,0 +1,20 @@
+function Bar() {
+  return (
+    <>
+      <div className=" flex flex-row justify-items-center max-w-full mx-10 space-x-7 p-10 bg-red-400 min-h-24">
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+        <div className=" w-32 bg-blue-100 h-32 "></div>
+      </div>
+    </>
+  );
+}
+
+export default Bar;
