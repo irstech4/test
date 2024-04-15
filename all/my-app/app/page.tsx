16a186365ee9481ce2ec1c9d5610d32733b13c34
IRS-Github <irs00tech04@gmail.com>
2024-04-15 16:54:45 +0530

    test checkout

--- all/my-app/app/page.tsx
+++ all/my-app/app/page.tsx
@@ -1,6 +1,11 @@
+import Cart from "@/components/cart/Cart"
+import Checkout from "@/components/checkout/Checkout"
+
 export default function Home() {
   return (
     <div>
+     <Cart/>
+     <Checkout/>
       <section>
         <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
           <div>
@@ -161,198 +166,96 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <div className='h-screen bg-gray-100 pt-20'>
-      <div className='mx-auto max-w-7xl justify-center px-6 md:flex md:space-x-6 xl:px-0'>
-        <div className='rounded-lg md:w-2/3 bg-gray-800'></div>
-        <div className=' mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3'></div>
-      </div>
 
-      <div className='h-screen bg-gray-100 pt-20'>
-        <h1 className='mb-10 text-center text-2xl font-bold'>Cart Items</h1>
-        <div className='mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0'>
-          <div className='rounded-lg md:w-2/3'>
-            <div className='justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start'>
-              <img
-                src='https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                alt='product-image'
-                className='w-full rounded-lg sm:w-40'
-              />
-              <div className='sm:ml-4 sm:flex sm:w-full sm:justify-between'>
-                <div className='mt-5 sm:mt-0'>
-                  <h2 className='text-lg font-bold text-gray-900'>
-                    Nike Air Max 2019
-                  </h2>
-                  <p className='mt-1 text-xs text-gray-700'>36EU - 4US</p>
+      <section>
+        <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
+          <div>
+            <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-5xl text-balance">
+              Building one pagers together
+              <span className="text-gray-600">wherever and anywhere</span>
+            </h1>
+            <p className="mt-4 text-base font-medium text-gray-500 text-balance">
+              Control and added security. With decentralization, users have more
+              control over their data and transactions, and the platform is less
+              susceptible to malicious attacks.
+            </p>
+            <div className="grid mt-12 gap-y-12 gap-x-2 md:grid-cols-2">
+              <div>
+                <div className="p-2 overflow-hidden border rounded-3xl">
+                  <img
+                    src="../images/placeholders/rectangle1.svg"
+                    alt="#_"
+                    className="w-full h-full mx-auto rounded-2xl"
+                  />
                 </div>
-                <div className='mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6'>
-                  <div className='flex items-center border-gray-100'>
-                    <span className='cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50'>
-                      {' '}
-                      -{' '}
-                    </span>
-                  
-                    <span className='cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50'>
-                      {' '}
-                      +{' '}
-                    </span>
-                  </div>
-                  <div className='flex items-center space-x-4'>
-                    <p className='text-sm'>259.000 ₭</p>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      stroke-width='1.5'
-                      stroke='currentColor'
-                      className='h-5 w-5 cursor-pointer duration-150 hover:text-red-500'
-                    >
-                      <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
-                        d='M6 18L18 6M6 6l12 12'
-                      />
-                    </svg>
-                  </div>
+                <div className="mt-4">
+                  <p className="font-medium text-gray-900">
+                    No warranty disclaimer
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    The license comes with no warranties. The licensor provides
+                    the work "as is," and users must use it at their own risk.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="p-2 overflow-hidden border rounded-3xl">
+                  <img
+                    src="../images/placeholders/rectangle1.svg"
+                    alt="#_"
+                    className="w-full h-full mx-auto rounded-2xl"
+                  />
+                </div>
+                <div className="px-8 mt-4">
+                  <p className="font-medium text-gray-900">
+                    Modification Freedom
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    You can adapt, remix, transform, and build upon the licensed
+                    work.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="p-2 overflow-hidden border rounded-3xl">
+                  <img
+                    src="../images/placeholders/rectangle1.svg"
+                    alt="#_"
+                    className="w-full h-full mx-auto rounded-2xl"
+                  />
+                </div>
+                <div className="px-8 mt-4">
+                  <p className="font-medium text-gray-900">
+                    Commercial use allowed
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    You are allowed to use the licensed work for both
+                    non-commercial and commercial purposes.
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div className="p-2 overflow-hidden border rounded-3xl">
+                  <img
+                    src="../images/placeholders/rectangle1.svg"
+                    alt="#_"
+                    className="w-full h-full mx-auto rounded-2xl"
+                  />
+                </div>
+                <div className="px-8 mt-4">
+                  <p className="font-medium text-gray-900">
+                    Share alike (SA) absence
+                  </p>
+                  <p className="mt-2 text-sm text-gray-500">
+                    The CC BY 3.0 License does not include a "Share Alike" (SA)
+                    provision.
+                  </p>
                 </div>
               </div>
             </div>
-            <div className='justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start'>
-              <img
-                src='https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80'
-                alt='product-image'
-                className='w-full rounded-lg sm:w-40'
-              />
-              <div className='sm:ml-4 sm:flex sm:w-full sm:justify-between'>
-                <div className='mt-5 sm:mt-0'>
-                  <h2 className='text-lg font-bold text-gray-900'>
-                    Nike Air Max 2019
-                  </h2>
-                  <p className='mt-1 text-xs text-gray-700'>36EU - 4US</p>
-                </div>
-                <div className='mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6'>
-                  <div className='flex items-center border-gray-100'>
-                    <span className='cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50'>
-                      {' '}
-                      -{' '}
-                    </span>
-            
-                    <span className='cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50'>
-                      {' '}
-                      +{' '}
-                    </span>
-                  </div>
-                  <div className='flex items-center space-x-4'>
-                    <p className='text-sm'>259.000 ₭</p>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      stroke-width='1.5'
-                      stroke='currentColor'
-                      className='h-5 w-5 cursor-pointer duration-150 hover:text-red-500'
-                    >
-                      <path
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
-                        d='M6 18L18 6M6 6l12 12'
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className='mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3'>
-            <div className='mb-2 flex justify-between'>
-              <p className='text-gray-700'>Subtotal</p>
-              <p className='text-gray-700'>$129.99</p>
-            </div>
-            <div className='flex justify-between'>
-              <p className='text-gray-700'>Shipping</p>
-              <p className='text-gray-700'>$4.99</p>
-            </div>
-            <hr className='my-4' />
-            <div className='flex justify-between'>
-              <p className='text-lg font-bold'>Total</p>
-              <div className=''>
-                <p className='mb-1 text-lg font-bold'>$134.98 USD</p>
-                <p className='text-sm text-gray-700'>including VAT</p>
-              </div>
-            </div>
-            <button className='mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600'>
-              Check out
-            </button>
           </div>
         </div>
-      </div>
-    </div>
-   
-      <section>
-                <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
-                  <div>
-                    <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-5xl text-balance">
-                      Building one pagers together
-                      <span className="text-gray-600">wherever and anywhere</span>
-                    </h1>
-                    <p className="mt-4 text-base font-medium text-gray-500 text-balance">
-                      Control and added security. With decentralization, users have more
-                      control over their data and transactions, and the platform is less
-                      susceptible to malicious attacks.
-                    </p>
-                    <div className="grid mt-12 gap-y-12 gap-x-2 md:grid-cols-2">
-                      <div>
-                        <div className="p-2 overflow-hidden border rounded-3xl">
-                          <img src="../images/placeholders/rectangle1.svg" alt="#_" className="w-full h-full mx-auto rounded-2xl"/>
-                        </div>
-                        <div className="mt-4">
-                          <p className="font-medium text-gray-900">No warranty disclaimer</p>
-                          <p className="mt-2 text-sm text-gray-500">
-                            The license comes with no warranties. The licensor provides the
-                            work "as is," and users must use it at their own risk.
-                          </p>
-                        </div>
-                      </div>
-                      <div>
-                        <div className="p-2 overflow-hidden border rounded-3xl">
-                          <img src="../images/placeholders/rectangle1.svg" alt="#_" className="w-full h-full mx-auto rounded-2xl"/>
-                        </div>
-                        <div className="px-8 mt-4">
-                          <p className="font-medium text-gray-900">Modification Freedom</p>
-                          <p className="mt-2 text-sm text-gray-500">
-                            You can adapt, remix, transform, and build upon the licensed work.
-                          </p>
-                        </div>
-                      </div>
-                      <div>
-                        <div className="p-2 overflow-hidden border rounded-3xl">
-                          <img src="../images/placeholders/rectangle1.svg" alt="#_" className="w-full h-full mx-auto rounded-2xl"/>
-                        </div>
-                        <div className="px-8 mt-4">
-                          <p className="font-medium text-gray-900">Commercial use allowed</p>
-                          <p className="mt-2 text-sm text-gray-500">
-                            You are allowed to use the licensed work for both non-commercial
-                            and commercial purposes.
-                          </p>
-                        </div>
-                      </div>
-                      <div>
-                        <div className="p-2 overflow-hidden border rounded-3xl">
-                          <img src="../images/placeholders/rectangle1.svg" alt="#_" className="w-full h-full mx-auto rounded-2xl"/>
-                        </div>
-                        <div className="px-8 mt-4">
-                          <p className="font-medium text-gray-900">Share alike (SA) absence</p>
-                          <p className="mt-2 text-sm text-gray-500">
-                            The CC BY 3.0 License does not include a "Share Alike" (SA)
-                            provision.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            
+      </section>
     </div>
   );
 }
